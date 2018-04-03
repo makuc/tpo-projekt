298ace94de4f5c799706d9c1988b404c39762f2d
Armin Makovec <armin.makovec@hotmail.com>
2018-04-03 15:50:15 +0000

    API klic za dodajanje zacetnih podatkov

--- app_api/routes/api.public.route.js
+++ app_api/routes/api.public.route.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 var ctrl = {
     other: require('../controllers/other.controller'),
+    populate: require('../controllers/populate.controller'),
     obcina: require("../controllers/obcina.controller")
 };
 
 
 router.get('/', ctrl.other.index);
-router.get('/test', ctrl.obcina.addObcinaTest);
+router.post('/db', ctrl.populate.vnosZacetnihPodatkov);
 
 module.exports = router;
