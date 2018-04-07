015f59937610409dff0e145bcadaf0a0e54c4b28
Uros Majcen <um2986@student.uni-lj.si>
2018-04-07 14:02:20 +0000

    Prirava logike za nogo, glavo in navigacijo; izdelava API-jev za studenta

---
+++ app_client/controllers/login.controller.js
@@ -0,0 +1,11 @@
+(function() {
+    /* global angular */
+    
+    function loginCtrl() {
+        var vm = this;
+    }
+    
+    angular
+        .module('tpo')
+        .contoller('loginCtrl', loginCtrl);
+})();
