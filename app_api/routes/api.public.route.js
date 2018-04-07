8a5dfbd9ae4aebb2b52576093a41f79bd693251d
Armin Makovec <armin.makovec@hotmail.com>
2018-04-07 16:06:25 +0000

    Poenotil API Routes

--- app_api/routes/api.public.route.js
+++ app_api/routes/api.public.route.js
@@ -5,7 +5,9 @@ var ctrl = {
     other: require('../controllers/other.controller'),
     populate: require('../controllers/populate.controller'),
     obcina: require("../controllers/obcina.controller"),
-    studenti: require("../controllers/studenti.controller")
+    studenti: require("../controllers/studenti.controller"),
+    
+    ctrlStudent: require("../controllers/student")
 };
 
 
@@ -13,5 +15,8 @@ router.get('/', ctrl.other.index);
 router.post('/db', ctrl.populate.vnosZacetnihPodatkov);
 router.delete('/db', ctrl.populate.izbrisBaze);
 router.get('/studenti', ctrl.studenti.getStudenti);
+
+router.get('/vsistudenti', ctrl.ctrlStudent.pridobiStudente);
+
 
 module.exports = router;
