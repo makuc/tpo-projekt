021ce2f513f388b544c4b5649f21f60f53f8373c
gucekpuhar <gucekenej@gmail.com>
2018-04-09 15:46:17 +0000

    Delo na login

--- app_client/controllers/login.controller.js
+++ app_client/controllers/login.controller.js
@@ -1,8 +1,46 @@
 (function() {
     /* global angular */
-    
-    function loginCtrl() {
+    loginCtrl.$inject = ["$location", "authentication", "$scope"];
+    function loginCtrl($location, authentication, $scope) {
         var vm = this;
+        
+        vm.loginData ={
+            e_posta: "",
+            password: ""
+            
+        };
+        
+        vm.sendData = function(){
+            if(!vm.loginData.e_posta || !vm.loginData.password){
+                
+                console.log("Zahtevana so vsa polja");
+                
+                return false;
+                
+            }else{
+                vm.login();
+                
+            }
+            
+            
+        };
+        
+        vm.login=function(){
+            console.log(vm.loginData);
+            authentication
+                .login(vm.loginData)
+                .then(function success() {
+                    $location.path("/vpisniList");
+                    
+                }, function error(error){
+                    
+                    console.log(error);
+                });
+            
+        };
+        
+        
+        
     }
     
     angular
