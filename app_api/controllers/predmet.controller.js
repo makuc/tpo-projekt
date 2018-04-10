763f8979d596d3abd6cb15624a9aac56f97a246e
Robert Barachini <robert.barachini@gmail.com>
2018-04-10 17:01:45 +0000

    Start work on predmet

--- app_api/controllers/predmet.controller.js
+++ app_api/controllers/predmet.controller.js
@@ -1,2 +1,22 @@
 var mongoose = require("mongoose");
 var Predmet = mongoose.model('Premdet');
+
+module.exports.pridobiPredmete = function(req, res) {
+  pridobiPredmete(req, res);
+};
+    
+function pridobiPredmete(req, res)
+{
+    var koncniObject = {};
+    Predmet
+        .find().limit(0)
+        .exec(function(err, predmeti) {
+            if(err) {
+                return res.status(404).send({ message: "Predmeti not found 1" });
+            }
+            if(!predmeti) return res.status(404).send({ message: "Predmeti not found 2" });
+            
+            koncniObject.predmeti = predmeti;
+            return res.status(200).json(koncniObject);
+    });
+}
