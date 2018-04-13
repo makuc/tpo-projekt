13142e1a87a3fc9341e71014aef8dae40a3b8dbd
gucekpuhar <gucekenej@gmail.com>
2018-04-13 08:36:58 +0000

    Vmesni komit

--- app_client/controllers/pozabljeno.controller.js
+++ app_client/controllers/pozabljeno.controller.js
@@ -7,16 +7,29 @@
         vm.email = "";
         
         vm.submit = function() {
+            if(!isEmail(vm.email))
+                return  vm.formError="Neveljaven elektronski naslov";
+            
             authentication.pozabljenoGeslo(vm.email).then(
                 function success(res) {
                     var pozabljenoId = res.data.ponastavi_geslo;
                     $location.path("/pozabljeno-geslo/"+pozabljenoId);
                 },
                 function error(res) {
-                    console.log("Napaka:\n" + res.data);
+                    vm.formError="Neveljaven uporabnik";
+                    
                 }
             );
         };
+        
+        
+        
+        
+        var isEmail = function(email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
+        };
+
         
     }
     
