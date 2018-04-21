5eeab1a9453023b99c7d4468216b0a873a20cba5
Armin Makovec <armin.makovec@hotmail.com>
2018-04-21 17:43:21 +0000

    Fixed zadeve, ki jih je Barachini-ju pokvarilo zaradi zaostanka veje

--- app_api/controllers/predmet.controller.js
+++ app_api/controllers/predmet.controller.js
@@ -342,28 +342,3 @@ function preveriIzvajalecZeIzvajaIzvedboPredmeta(req, res, next) {
   
   callNext(req, res, next);
 }
-var Predmet = mongoose.model('Predmet');
-var Student = mongoose.model("Student");
-
-module.exports.pridobiStudente = function(req, res)
-{
-    pridobiStudente(req, res);
-};
-    
-function pridobiStudente(req, res)
-{
-    var koncniObject = {};
-    var studijskoLeto_id = req.params.studijskoLeto_id;
-    var predmet_id = req.params.predmet_id;
-    Predmet
-        .find().limit(0)
-        .exec(function(err, predmeti) {
-            if(err) {
-                return res.status(404).send({ message: "Predmeti not found 1" });
-            }
-            if(!predmeti) return res.status(404).send({ message: "Predmeti not found 2" });
-            
-            koncniObject.predmeti = predmeti;
-            return res.status(200).json(koncniObject);
-    });
-};
