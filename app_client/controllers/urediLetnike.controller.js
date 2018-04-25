1e6c1c348ebb2d4e9283dd7fa9a850bb6d542e2f
Robert Barachini <robert.barachini@gmail.com>
2018-04-25 07:04:25 +0000

    Urejanje letnikov

---
+++ app_client/controllers/urediLetnike.controller.js
@@ -0,0 +1,52 @@
+(function() {
+    /* global angular */
+    
+    urediLetnikeCtrl.$inject = ['ostaloPodatki', '$location'];
+    
+    function urediLetnikeCtrl(ostaloPodatki, $location){
+        var vm = this;
+        
+        vm.prikaziLetnike = function(){
+            ostaloPodatki.pridobiVseLetnike().then(
+                function success(odgovor){
+                    vm.letniki = odgovor.data;
+                },
+                function error(odgovor){
+                    console.log(odgovor);
+                }
+            );
+        };
+        
+        vm.izbris = function(letnikId){
+            ostaloPodatki.izbrisiLetnik(letnikId).then(
+                function success(odgovor){
+                    vm.prikaziLetnike();
+                },
+                function error(odgovor){
+                    console.log(odgovor);
+                }
+            );
+        };
+        
+        vm.obnovi = function(letnikId){
+            ostaloPodatki.obnoviLetnik(letnikId).then(
+                function success(odgovor){
+                    vm.prikaziLetnike();
+                },
+                function error(odgovor){
+                    console.log(odgovor);
+                }
+            );
+        };
+        
+        vm.uredi = function(letnikId){
+            $location.path("/urediLetnik/" + letnikId);
+        };
+        
+    }
+    
+    angular
+        .module('tpo')
+        .controller('urediLetnikeCtrl', urediLetnikeCtrl);
+    
+})();
