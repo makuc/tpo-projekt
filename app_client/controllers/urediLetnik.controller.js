f4d83ef0dc145603908d96389000133a9e46a8ee
Armin Makovec <armin.makovec@hotmail.com>
2018-04-25 14:09:41 +0000

    Popravek urediLetnik.controller

--- app_client/controllers/urediLetnik.controller.js
+++ app_client/controllers/urediLetnik.controller.js
@@ -1,54 +1,72 @@
 (function() {
-    /* global angular */
+  /* global angular */
+  
+  urediLetnikCtrl.$inject = ['$location', 'ostaloPodatki', '$routeParams'];
+  
+  
+  function urediLetnikCtrl($location, ostaloPodatki, $routeParams){
+    var vm = this;
     
-    urediLetnikCtrl.$inject = ['$location', 'ostaloPodatki', '$routeParams'];
+    vm.idLetnika = $routeParams.idLetnika;
+    
+    //vm.opcijeKT = ["a", "b", "c"];
+    //vm.opcijeKT = [1,2,3,4,5,6];
+    ostaloPodatki
+      .pridobiVseVeljavneStudijskePrograme()
+      .then(
+        function success(odgovor) {
+          vm.studijskiProgrami = odgovor.data;
+        },
+        function error(odgovor) {
+          console.log(odgovor);
+        }
+      );
+    ostaloPodatki
+      .pridobiVseVeljavneLetnike()
+      .then(
+        function success(odgovor) {
+          vm.pogoji = odgovor.data;
+        },
+        function error(odgovor) {
+          console.log(odgovor);
+        }
+      );
     
     
-    function urediLetnikCtrl($location, ostaloPodatki, $routeParams){
-        var vm = this;
-        
-        vm.idLetnika = $routeParams.idLetnika;
-        
-        //vm.opcijeKT = ["a", "b", "c"];
-        //vm.opcijeKT = [1,2,3,4,5,6];
-        vm.studijskiProgrami = ostaloPodatki.pridobiVseVeljavneStudijskePrograme();
-        vm.pogoji = ostaloPodatki.pridobiVseVeljavneLetnike();
-        
-        vm.pridobiLetnik = function(){
-            ostaloPodatki.najdiLetnik(vm.idLetnika).then(
-                function success(odgovor){
-                  
-                    vm.letnik = odgovor.data;
-                },
-                function error(odgovor){
-                    console.log(odgovor);
-                }
-            );
-        };
-        
-        vm.shrani = function(){
-            var letnik = {
-                studijskiProgram: vm.letnik.studijskiProgram,
-                pogoj_letnik: vm.letnik.pogoj_letnik,
-                naziv: vm.letnik.naziv
-            };
-            ostaloPodatki.urediLetnik(vm.idLetnika, letnik).then(
-                function success(odgovor){
-                    $location.path("/urediLetnike/");
-                },
-                function error(odgovor){
-                    vm.obvestilo = "Že obstaja zapis s to šifro";
-                    console.log(odgovor);
-                }
-            );
-        };
-        
-        vm.preklici = function(){
-            $location.path("/urediLetnike/");
-        };
-    }
+    vm.pridobiLetnik = function(){
+      ostaloPodatki.najdiLetnik(vm.idLetnika).then(
+        function success(odgovor){
+          vm.letnik = odgovor.data;
+        },
+        function error(odgovor){
+          console.log(odgovor);
+        }
+      );
+    };
     
-    angular
-        .module('tpo')
-        .controller('urediLetnikCtrl', urediLetnikCtrl);
+    vm.shrani = function(){
+      var letnik = {
+        studijskiProgram: vm.letnik.studijskiProgram,
+        pogoj_letnik: vm.letnik.pogoj_letnik,
+        naziv: vm.letnik.naziv
+      };
+      ostaloPodatki.urediLetnik(vm.idLetnika, letnik).then(
+        function success(odgovor){
+          $location.path("/urediLetnike/");
+        },
+        function error(odgovor){
+          vm.obvestilo = "Že obstaja zapis s to šifro";
+          console.log(odgovor);
+        }
+      );
+    };
+    
+    vm.preklici = function(){
+      $location.path("/urediLetnike/");
+    };
+  }
+  
+  angular
+    .module('tpo')
+    .controller('urediLetnikCtrl', urediLetnikCtrl);
 })();
