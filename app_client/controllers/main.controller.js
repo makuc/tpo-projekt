edc279b969207d58a2d6d7b5bb50424d7b0ccdc8
Uros Majcen <um2986@student.uni-lj.si>
2018-05-07 20:08:08 +0000

    Dodan gumb za vpis v meniju študenta, usposobljen gumb prekliči pri vpisnem listu

--- app_client/controllers/main.controller.js
+++ app_client/controllers/main.controller.js
@@ -1,7 +1,28 @@
 (function() {
     /* global angular */
-    mainCtrl.$inject = ['$location', 'authentication', '$scope','$route','$window','$http'];
-    function mainCtrl($location, authentication, $scope, $route, $window, $http) {
+    mainCtrl.$inject = ['$location', 'authentication', '$scope','$route','$window','$http', 'studentPodatki'];
+    function mainCtrl($location, authentication, $scope, $route, $window, $http, studentPodatki) {
+        var vm = this;
+        
+        studentPodatki.izpisStudenta(authentication.currentUser().student).then(
+            function succer(odgovor){
+                for(var i = 0; i < odgovor.data.zetoni.length; i++){
+                    if(!odgovor.data.zetoni[0].izkoriscen){
+                        vm.neizkoriscenZeton = true;
+                    }
+                }
+            },
+            function error(odgovor){
+                console.log(odgovor);
+            }
+        );
+        
+        vm.vpisniList = function(){
+          $location.path('/vpis/' + authentication.currentUser().student + '/podatkiStudenta');  
+        };
+        
+        console.log();
+        
         if(!authentication.auth()) {
             return $location.path('/login');
         }
