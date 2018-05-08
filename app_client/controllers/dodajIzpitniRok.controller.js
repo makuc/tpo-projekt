b626fec165d75e5547f2c1c85d078d03db6831b2
Uros Majcen <um2986@student.uni-lj.si>
2018-05-08 18:28:50 +0000

    Narejeno dodajanje in prikaz izpitnih rokov za referentko

--- app_client/controllers/dodajIzpitniRok.controller.js
+++ app_client/controllers/dodajIzpitniRok.controller.js
@@ -7,6 +7,47 @@
     function dodajIzpitniRokCtrl($location, ostaloPodatki, $routeParams, predmetPodatki, izpitniRokPodatki){
         var vm = this;
         
+        function preveriDatum(datum){
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+
+            if(yyyy < datum.getFullYear()){
+                if(datum.getDay() > 5){
+                    vm.obvestilo = "Med vikendi ni mogoče dodanjanje izpitnega roka";
+                    return false;
+                }
+                return true;
+            } else if(yyyy == datum.getFullYear()){
+                if(mm < datum.getMonth()+1){
+                    if(datum.getDay() > 5){
+                        vm.obvestilo = "Med vikendi ni mogoče dodanjanje izpitnega roka";
+                        return false;
+                    }
+                    return true;
+                } else if(mm == datum.getMonth()+1){
+                    if(dd < datum.getDate()){
+                        if(datum.getDay() > 5){
+                            vm.obvestilo = "Med vikendi ni mogoče dodanjanje izpitnega roka";
+                            return false;
+                        }
+                        return true;
+                    } else {
+                        vm.obvestilo = "Datum je manjši kot današnji";
+                        return false;
+                    }
+                } else {
+                    vm.obvestilo = "Datum je manjši kot današnji";
+                    return false;
+                }
+            } else {
+                vm.obvestilo = "Datum je manjši kot današnji";
+                return false;
+            }
+        }
+
+        
         predmetPodatki.izpisiVseVeljavnePredmete().then(
             function success(odgovor){
                 vm.predmeti = odgovor.data;
@@ -28,22 +69,31 @@
         
         vm.shrani = function(){
             //preveri veljavnost datuma
-            var data = {
-                predmet: vm.podatki.predmet,
-                studijsko_leto: vm.podatki.studijsko_leto,
-                datum_izvajanja: vm.podatki.datum_izvajanja,
-                opombe: vm.podatki.opombe
-            };
             
-            izpitniRokPodatki.ustvariIzpitniRok(data).then(
-                function success(odgovor){
-                    $location.path("/vsiIzpitniRoki");
-                },
-                function error(odgovor){
-                    vm.obvestilo = "Napaka";
-                    console.log(odgovor);
-                }
-            );
+            var datumOk = preveriDatum(vm.podatki.datum);
+            
+            if(datumOk){
+                var data = {
+                predmet: vm.podatki.predmet,
+                studijsko_leto: vm.podatki.studijskoLeto,
+                datum_izvajanja: vm.podatki.datum,
+                opombe: vm.podatki.opombe
+                };
+    
+            
+                izpitniRokPodatki.ustvariIzpitniRok(data).then(
+                    function success(odgovor){
+                        $location.path("/vsiIzpitniRoki");
+                    },
+                    function error(odgovor){
+                        vm.obvestilo = odgovor.data.message;
+                        console.log(odgovor);
+                    }
+                ); 
+            }
+                
+            
+
         };
         
         vm.preklici = function(){
