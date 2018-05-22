5c654c21925208c1511bc35d08b73689f33d8d7e
Armin Makovec <armin.makovec@hotmail.com>
2018-05-22 08:24:58 +0000

    Revert "Dokončano urejanje datuma... :P"
    This reverts commit c16addd3de9fc19ec5178d82b60b8d10b18d6c2b.

--- app_client/app.client.js
+++ app_client/app.client.js
@@ -452,7 +452,32 @@
         
         // Register interceptors
         .factory('httpRequestInterceptor', httpInterceptor)
-        .config(httpInterceptorConfig);
+        .config(httpInterceptorConfig)
+        .config(['momentPickerProvider', function (momentPickerProvider) {
+            momentPickerProvider.options({
+                /* Picker properties */
+                locale:        'sl',
+                format:        'L LTS',
+                minView:       'decade',
+                maxView:       'minute',
+                startView:     'year',
+                autoclose:     true,
+                today:         false,
+                keyboard:      false,
+                
+                /* Extra: Views properties */
+                leftArrow:     '&larr;',
+                rightArrow:    '&rarr;',
+                yearsFormat:   'YYYY',
+                monthsFormat:  'MMM',
+                daysFormat:    'D',
+                hoursFormat:   'HH:[00]',
+                minutesFormat: moment.localeData().longDateFormat('LT').replace(/[aA]/, ''),
+                secondsFormat: 'ss',
+                minutesStep:   5,
+                secondsStep:   1
+            });
+        }]);
         //.config(dateLocaleProviderConfig);
     
     function httpInterceptorConfig($httpProvider) {
